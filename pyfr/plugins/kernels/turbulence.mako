5a9124a372e3cb7e3a5afc6c9f5834727131cbc1
Peter Vincent <p.vincent@imperial.ac.uk>
2022-06-08 18:09:56 +0100

    Progress.

--- pyfr/plugins/kernels/turbulence.mako
+++ pyfr/plugins/kernels/turbulence.mako
@@ -9,12 +9,15 @@
   fpdtype_t r2;
   % for j in range(nvmax):
     r2 = 0.0;
-    % for i in range(ndims):
-        r2 += (acteddy[${j}][${i}]-ploc[${i}])*(acteddy[${j}][${i}]-ploc[${i}]);
-    % endfor
-    src[0] += r2 < rad*rad ? acteddy[${j}][2] : 0.0;
-    src[1] += r2 < rad*rad ? acteddy[${j}][2]*(acteddy[${j}][1]-ploc[1]) : 0.0;
-    src[2] += r2 < rad*rad ? -acteddy[${j}][2]*(acteddy[${j}][0]-ploc[0]) : 0.0;
-    src[3] += r2 < rad*rad ? acteddy[${j}][2] : 0.0;
+    if (t > acteddy[${j}][3] && t < acteddy[${j}][4])
+    {
+        % for i in range(ndims):
+            r2 += (acteddy[${j}][${i}]-ploc[${i}])*(acteddy[${j}][${i}]-ploc[${i}]);
+        % endfor
+        src[0] += r2 < rad*rad ? acteddy[${j}][2] : 0.0;
+        src[1] += r2 < rad*rad ? acteddy[${j}][2]*(acteddy[${j}][1]-ploc[1]) : 0.0;
+        src[2] += r2 < rad*rad ? -acteddy[${j}][2]*(acteddy[${j}][0]-ploc[0]) : 0.0;
+        src[3] += r2 < rad*rad ? acteddy[${j}][2] : 0.0;
+    }
   % endfor
 </%pyfr:macro>
--- pyfr/plugins/turbulence.py
+++ pyfr/plugins/turbulence.py
@@ -12,18 +12,24 @@ class Turbulence(BasePlugin):
     def __init__(self, intg, cfgsect, suffix):
         super().__init__(intg, cfgsect, suffix)
         
-        self.nvmax = nvmax = 5
-        self.nparams = nparams = 3
+        self.nvmax = nvmax = 1
+        self.nparams = nparams = 5
         
         self.acteddy = acteddy = {}
         self.neles = neles = {}
         
         self.vorts = []
-        self.vorts.append({'xinit': -5.0, 'yinit': -5.0, 'x': 0.0, 'eps': 0.1})
-        self.vorts.append({'xinit': -2.0, 'yinit': -3.0, 'x': 0.0, 'eps': 0.1})
-        self.vorts.append({'xinit': -3.0, 'yinit': 4.0, 'x': 0.0, 'eps': 0.1})
-        self.vorts.append({'xinit': -9.0, 'yinit': 0.0, 'x': 0.0, 'eps': 0.1})
-        self.vorts.append({'xinit': -7.0, 'yinit': 2.0, 'x': 0.0, 'eps': 0.1})
+        self.vorts.append({'xinit': -5.0, 'yinit': -5.0, 'x': 0.0, 'eps': 0.1, 'ts': 0.0, 'te': 50.0})
+        
+        #self.vorts.append({'xinit': -2.0, 'yinit': -3.0, 'x': 0.0, 'eps': 0.1})
+        #self.vorts.append({'xinit': -3.0, 'yinit': 4.0, 'x': 0.0, 'eps': 0.1})
+        #self.vorts.append({'xinit': -9.0, 'yinit': 0.0, 'x': 0.0, 'eps': 0.1})
+        #self.vorts.append({'xinit': -7.0, 'yinit': 2.0, 'x': 0.0, 'eps': 0.1})
+        #self.vorts.append({'xinit': -6.0, 'yinit': -9.0, 'x': 0.0, 'eps': 0.1})
+        #self.vorts.append({'xinit': -5.0, 'yinit': -8.0, 'x': 0.0, 'eps': 0.1})
+        #self.vorts.append({'xinit': -9.0, 'yinit': 2.0, 'x': 0.0, 'eps': 0.1})
+        #self.vorts.append({'xinit': -3.0, 'yinit': -4.0, 'x': 0.0, 'eps': 0.1})
+        #self.vorts.append({'xinit': -2.0, 'yinit': 5.0, 'x': 0.0, 'eps': 0.1})
         #self.vorts.append({'x': -3.0, 'y': -3.0, 'eps': 1.0})
         self.vortrad = vortrad = 1.0
         
@@ -57,5 +63,7 @@ class Turbulence(BasePlugin):
     	            temp[vortid,0,eid]=vort['x']
     	            temp[vortid,1,eid]=vort['yinit']
     	            temp[vortid,2,eid]=vort['eps']
+    	            temp[vortid,3,eid]=vort['ts']
+    	            temp[vortid,4,eid]=vort['te']
     	    self.acteddy[etype].set(temp)
     	    
