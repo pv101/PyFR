59cf6eab030539b35f546509eb3d694f8e875be9
Peter Vincent <p.vincent@imperial.ac.uk>
2022-06-10 12:51:45 +0100

    Starting to generalise vortex addition.

--- pyfr/plugins/kernels/turbulence.mako
+++ pyfr/plugins/kernels/turbulence.mako
@@ -2,24 +2,30 @@
 <%namespace module='pyfr.backends.base.makoutil' name='pyfr'/>
 
 <%pyfr:macro name='turbulence' params='t, u, ploc, src'>
-  % for i in range(nvars):
-    src[${i}] += 0.0;
-  % endfor
   fpdtype_t rad = ${vortrad};
+  fpdtype_t xvel = ${xvel};
   fpdtype_t r2;
+  fpdtype_t pos[${ndims}];
   % for j in range(nvmax):
     r2 = 0.0;
-    if (t > acteddy[${j}][4] && t < acteddy[${j}][5])
+    if (t > acteddy[${j}][5] && t < acteddy[${j}][6])
     {
+        pos[0] = acteddy[${j}][0] + (t-acteddy[${j}][3])*xvel;
+        pos[1] = acteddy[${j}][1];
+        
+        % if ndims == 3:
+            pos[2] = acteddy[${j}][2];
+        % endif
+        
         % for i in range(ndims):
-            r2 += (acteddy[${j}][${i}]-ploc[${i}])*(acteddy[${j}][${i}]-ploc[${i}]);
+            r2 += (pos[${i}]-ploc[${i}])*(pos[${i}]-ploc[${i}]);
         % endfor
         
-        src[0] += r2 < rad*rad ? acteddy[${j}][3] : 0.0;
+        src[0] += r2 < rad*rad ? acteddy[${j}][4] : 0.0;
         % for i in range(ndims):
-            src[${i+1}] += r2 < rad*rad ? acteddy[${j}][3]*(acteddy[${j}][${i}]-ploc[${i}]) : 0.0;
+            src[${i+1}] += r2 < rad*rad ? acteddy[${j}][4]*(pos[${i}]-ploc[${i}]) : 0.0;
         % endfor
-        src[${nvars-1}] += r2 < rad*rad ? acteddy[${j}][3] : 0.0;
+        src[${nvars-1}] += r2 < rad*rad ? acteddy[${j}][4] : 0.0;
     }
   % endfor
 </%pyfr:macro>
--- pyfr/plugins/turbulence.py
+++ pyfr/plugins/turbulence.py
@@ -12,59 +12,64 @@ class Turbulence(BasePlugin):
     def __init__(self, intg, cfgsect, suffix):
         super().__init__(intg, cfgsect, suffix)
         
+        self.xvel = xvel = 0.5
+        
+        self.nvorts = 20
+        
+        # the box
+        self.xmin = 0.4
+        self.xmax = 0.6
+        self.ymin = 0.0
+        self.ymax = 1.0
+        self.zmin  =0.0
+        self.zmax = 1.0
+        
         self.nvmax = nvmax = 1
-        self.nparams = nparams = 6
+        self.nparams = nparams = 7
         
         self.acteddy = acteddy = {}
         self.neles = neles = {}
         
         self.vorts = []
-        self.vorts.append({'xinit': 0.3, 'yinit': 0.5, 'zinit': 0.5, 'x': 0.0, 'eps': 0.01, 'ts': 0.0, 'te': 50.0})
+        eles = []
         
-        #self.vorts.append({'xinit': -2.0, 'yinit': -3.0, 'x': 0.0, 'eps': 0.1})
-        #self.vorts.append({'xinit': -3.0, 'yinit': 4.0, 'x': 0.0, 'eps': 0.1})
-        #self.vorts.append({'xinit': -9.0, 'yinit': 0.0, 'x': 0.0, 'eps': 0.1})
-        #self.vorts.append({'xinit': -7.0, 'yinit': 2.0, 'x': 0.0, 'eps': 0.1})
-        #self.vorts.append({'xinit': -6.0, 'yinit': -9.0, 'x': 0.0, 'eps': 0.1})
-        #self.vorts.append({'xinit': -5.0, 'yinit': -8.0, 'x': 0.0, 'eps': 0.1})
-        #self.vorts.append({'xinit': -9.0, 'yinit': 2.0, 'x': 0.0, 'eps': 0.1})
-        #self.vorts.append({'xinit': -3.0, 'yinit': -4.0, 'x': 0.0, 'eps': 0.1})
-        #self.vorts.append({'xinit': -2.0, 'yinit': 5.0, 'x': 0.0, 'eps': 0.1})
-        #self.vorts.append({'x': -3.0, 'y': -3.0, 'eps': 1.0})
         self.vortrad = vortrad = 0.05
         
+        # add a single vortex
+        self.vorts.append({'xinit': 0.3, 'yinit': 0.5, 'zinit': 0.5, 'tinit': 0.0, 'eps': 0.01, 'eles': {}})
+        
+        # add elements to vortices
+        for vort in self.vorts:
+            for etype, eles in intg.system.ele_map.items():
+                vort['eles'][etype] = []
+                for eid in range(eles.neles):
+                    vort['eles'][etype].append({'ts': 0.0, 'te': 50.0})
+        
+        # add macro and external data  
         for etype, eles in intg.system.ele_map.items():
-            eles.add_src_macro('pyfr.plugins.kernels.turbulence','turbulence', {'nvmax': nvmax, 'vortrad': vortrad})
+            eles.add_src_macro('pyfr.plugins.kernels.turbulence','turbulence', {'nvmax': nvmax, 'vortrad': vortrad, 'xvel': xvel})
             acteddy[etype] = eles._be.matrix((nvmax, nparams, eles.neles), tags={'align'})
-            neles[etype] = eles.neles
             eles._set_external('acteddy',
                                f'in broadcast-col fpdtype_t[{nvmax}][{nparams}]',
                                value=acteddy[etype])
-                      
-        #for etype, eles in intg.system.ele_map.items():
-    	#    print('Inital data')
-    	#    temp = np.zeros((self.nvmax, self.nparams, eles.neles))
-    	#    for eid in range(eles.neles):
-    	#        for vortid, vort in enumerate(self.vorts):
-    	#            temp[vortid,0,eid]=vort['x']
-    	#            temp[vortid,1,eid]=vort['y']
-    	#            temp[vortid,2,eid]=vort['eps']
-    	#    self.acteddy[etype].set(temp)            
-        
+                               
+        # keep info for later                       
+        for etype, eles in intg.system.ele_map.items():
+            neles[etype] = eles.neles
+                                 
     def __call__(self, intg):
         tcurr = intg.tcurr
-        for vort in self.vorts:
-            vort['x'] = vort['xinit']+0.5*tcurr
-
+        
         for etype, neles in self.neles.items():
     	    temp = np.zeros((self.nvmax, self.nparams, neles))
     	    for eid in range(neles):
     	        for vortid, vort in enumerate(self.vorts):
-    	            temp[vortid,0,eid]=vort['x']
+    	            temp[vortid,0,eid]=vort['xinit']
     	            temp[vortid,1,eid]=vort['yinit']
     	            temp[vortid,2,eid]=vort['zinit']
-    	            temp[vortid,3,eid]=vort['eps']
-    	            temp[vortid,4,eid]=vort['ts']
-    	            temp[vortid,5,eid]=vort['te']
+    	            temp[vortid,3,eid]=vort['tinit']
+    	            temp[vortid,4,eid]=vort['eps']
+    	            temp[vortid,5,eid]=vort['eles'][etype][eid]['ts']
+    	            temp[vortid,6,eid]=vort['eles'][etype][eid]['te']
     	    self.acteddy[etype].set(temp)
     	    
