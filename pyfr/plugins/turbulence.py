50a2f1841bd4bf166747664bbdd50648fdf03c56
Peter Vincent <p.vincent@imperial.ac.uk>
2022-07-01 22:18:44 +0100

    Cleanups.

--- pyfr/plugins/turbulence.py
+++ pyfr/plugins/turbulence.py
@@ -107,38 +107,7 @@ class Turbulence(BasePlugin):
             t += tdead
             
         self.vorts.append(vort_chain)
-    
-    def remove_vort_from_chain(self, intg, vcid, vid):
-        #print(self.vorts[vcid])
-        self.vorts[vcid][vid]['killcount'] += 1
-        #print(self.vorts[vcid][vid]['killcount'])
-        # if all the element types are done with it
-        if self.vorts[vcid][vid]['killcount'] == self.n_ele_types:
-            #print(f"Killing {vid} from chain {vcid}.")
-            del self.vorts[vcid][vid] # delete
-            #print(self.vorts[vcid])
-            # then add one to end of the chain
-            t = self.vorts[vcid][next(reversed(self.vorts[vcid]))]['tdead']
-            seed = int(t*10000)
-            
-            #print(f'vcid={vcid}, seed={t}')
-            np.random.seed(seed)
-            xyz = np.random.uniform(0, 1, 3)
-            
-            xinit = self.xmin + self.vortrad
-            yinit = (self.ymin + self.vortrad) + (self.ymax-self.ymin-2*self.vortrad)*xyz[1]
-            zinit = (self.zmin + self.vortrad) + (self.zmax-self.zmin-2*self.vortrad)*xyz[2]
-            
-            #print(f'x={xinit}, y={yinit}, z={zinit}')
-            
-            tdead = t + (self.xmax-xinit-self.vortrad)/self.xvel
-            
-            vid = uuid.uuid1()
-            
-            vort = {'killcount': 0, 'vcid': vcid, 'vid': vid, 'xinit': xinit, 'yinit': yinit, 'zinit': zinit, 'tinit': t, 'tdead': tdead, 'eps': 0.01}
-            self.vorts[vcid][vid]=vort
-            self.vort_to_buffer(intg, vort)
-            
+           
     def add_vort_to_chain(self, intg, vcid):
         t = self.vorts[vcid][next(reversed(self.vorts[vcid]))]['tdead']
         seed = int(t*10000)
@@ -185,35 +154,31 @@ class Turbulence(BasePlugin):
                 te = min(vort['tdead'], ts + (xmax-xmin+2*self.vortrad)/self.xvel)
                 self.buff[etype].append({'action': 'push', 'vcid': vort['vcid'], 'vid': vort['vid'], 'eid': eid, 'ts': ts, 'te': te, 't': ts}) 
             
-            self.buff[etype].append({'action': 'dead', 'vcid': vort['vcid'], 'vid': vort['vid'], 'ts': vort['tdead'], 'te': vort['tdead'], 't': vort['tdead']}) 
+            self.buff[etype].append({'action': 'dead', 'vcid': vort['vcid'],
+                                     'vid': vort['vid'], 'ts': vort['tdead'],
+                                     'te': vort['tdead'], 't': vort['tdead']}) 
             self.buff[etype].sort(key=lambda x: x["t"])
                                     
     def __call__(self, intg):
-        tcurr = intg.tcurr
-        gmin = 0
-        breaketype = None
-        breakeid = None
-        breaktime = []
-        #print(f"Hello, {tcurr}. You are 1.")
-        if tcurr >= self.tnext:
         
+        tcurr = intg.tcurr
+        tfull = []
+        tfullm = 0
+        
+        if tcurr >= self.tnext:
             for etype, neles in self.neles.items():
                 temp = np.zeros((self.nvmax, self.nparams, neles))
-                ctemp = np.zeros(neles).astype(int) # keep track of which vortex entry we are at for each element
-                #print(etype)
-                self.buffloc[etype] = 0
+                # keep track of which vortex entry we are at for each element
+                ctemp = np.zeros(neles).astype(int)
+                # position along buffer 
+                buffloc = 0
                 while self.buff[etype]:
-                    #act = self.buff[etype].pop(0)
-                    act = self.buff[etype][self.buffloc[etype]]
-                    self.buffloc[etype] += 1
+                    act = self.buff[etype][buffloc]
+                    buffloc += 1
                     if act['action'] == 'push':
+        	            eid = act['eid']
         	            vcid = act['vcid']
         	            vid = act['vid']
-        	            eid = act['eid']
-        	            te = act['te']
-        	            #print(f'VCID: {vcid}')
-        	            print(f'vcid = {vcid}')
-        	            print(f'te = {te}')
         	            vort = self.vorts[vcid][vid]
         	            temp[ctemp[eid],0,eid]=vort['xinit']
         	            temp[ctemp[eid],1,eid]=vort['yinit']
@@ -224,86 +189,28 @@ class Turbulence(BasePlugin):
         	            temp[ctemp[eid],6,eid]=act['te']
         	            ctemp[eid] += 1
         	            if ctemp[eid] == self.nvmax:
-        	                #print(ctemp[eid])
-        	                #print(etype)
-        	                #print(eid)
-        	                print(act['ts'])
-        	                breaketype = etype
-        	                breakeid = eid
-        	                breaktime.append(act['ts']) #?
+        	                # record ts associated with maxed out bufer for given etype
+        	                tfull.append(act['ts'])
+        	                # break the while loop for that particular element type
         	                break
                     elif act['action'] == 'dead':
     	                vcid = act['vcid']
-    	                vid = act['vid']
+    	                # add a new vortx to the chain so it doesnt risk running dry
     	                self.add_vort_to_chain(intg, vcid)
+                
+                # send off the active eddy array
                 self.acteddy[etype].set(temp)
-                #print(f'rank={self.rank} and min = {min(breaktime)}')
-                gmin = self.comm.allreduce(min(breaktime), op=MPI.MIN)
-                #print(f'rank={self.rank} and globalmin = {gmin}')
-                print(gmin)
-                #ci = self.buff[etype].index(next(obj for obj in self.buff[etype] if obj['te'] >= gmin))
-                self.buff[etype] = list(filter(lambda x: x['te'] >= gmin, self.buff[etype]))
-                #self.buff[etype]=self.buff[etype][ci:]
                 
+                # agree on how far we can go
+                tfullm = self.comm.allreduce(min(tfull), op=MPI.MIN)
                 
-                te0=self.buff[etype][0]['te']
-                print(f'te0 = {te0}')
-
-                # finally cull vorts from vortbuffer
+                # cull old actions
+                self.buff[etype] = list(filter(lambda x: x['te'] >= tfullm, self.buff[etype]))
+            
+                # cull old vorts
                 for vortchain in self.vorts:
-                    while list(vortchain.values())[0]['tdead'] < gmin:
-                        if(self.rank == 0):
-                            tdead = list(vortchain.values())[0]['tdead']
-                            vcid = list(vortchain.values())[0]['vcid']
-                            print(f'front tdead of vcid {vcid} = {tdead} cf. {gmin}') 
-                        popped = vortchain.popitem(last=False)
-                        #pvid = popped['vid']
-                        #pvcid = popped['vcid']
-                        #print(f'popped {vid} from vc {vcid}')
-                        if(self.rank == 0):
-                            print(popped[0])
-                            tdead = list(vortchain.values())[0]['tdead']
-                            vcid = list(vortchain.values())[0]['vcid']
-                            print(f'DONE front tdead of vcid {vcid} = {tdead} cf. {gmin}')
-
-        	    # need to store the temp packed buffer, so we can cull old stuff but keep relevant stuff on next refill        
-        
-        
-        
-            print('Transferring update ...')
-            #print(self.vorts)
-            #print(self.buff)
-            #for etype, neles in self.neles.items():
-                #uuid = list(self.vorts[0].values())[0]['vid']
-                #print(uuid)
-                #self.remove_vort_from_chain(intg, 0, uuid)
-          
-            #for etype, neles in self.neles.items():
-
-        	#    temp = np.zeros((self.nvmax, self.nparams, neles))
-        	#    ctemp = np.zeros(neles).astype(int) # keep track of which vortex entry we are at for each element
-
-        	#    for vort in self.vorts:
-        	#        elestemp = vort['eles'][etype]
-        	#        for ele in elestemp:
-        	#            eid = ele[0]
-        	#            ts = ele[1]
-        	#            te = ele[2]
-        	#            if ts < (self.tnext+self.twin) and te > self.tnext:
-        	#                if ctemp[eid] >= self.nvmax:
-        	#                    print("Error ...")
-        	#                temp[ctemp[eid],0,eid]=vort['xinit']
-        	#                temp[ctemp[eid],1,eid]=vort['yinit']
-        	#                temp[ctemp[eid],2,eid]=vort['zinit']
-        	#                temp[ctemp[eid],3,eid]=vort['tinit']
-        	#                temp[ctemp[eid],4,eid]=vort['eps']  
-        	#                temp[ctemp[eid],5,eid]=ts
-        	#                temp[ctemp[eid],6,eid]=te
-        	#                ctemp[eid] += 1
-        	#            else:
-        	#                print('rejecting')
-        	                
-        	#    self.acteddy[etype].set(temp)
-        
-            self.tnext = gmin
+                    while list(vortchain.values())[0]['tdead'] < tfullm:
+                        vortchain.popitem(last=False)
+                        
+            self.tnext = tfullm
     	    
