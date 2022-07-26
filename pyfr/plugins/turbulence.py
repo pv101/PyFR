04147d98d887847880f9cabe836909df2826ed52
Peter Vincent <p.vincent@imperial.ac.uk>
2022-07-26 21:40:16 +0100

    More cleanups.

--- pyfr/plugins/turbulence.py
+++ pyfr/plugins/turbulence.py
@@ -53,35 +53,31 @@ class Turbulence(BasePlugin):
         self.nvmax = nvmax = 15
         self.nparams = nparams = 9
         
-        self.buffloc = {}  
+        self.vortchains = []
         
-        self.mesh = intg.system.ele_map.items()
-        
-        self.acteddy = acteddy = {}
-        self.neles = neles = {}
-        
-        self.vorts = []
-        eles = []
-
         self.buff = {}
-        
-        self.n_ele_types = len(intg.system.ele_map)
-        
         for etype in intg.system.ele_map:
             self.buff[etype] = []
         
-        eset = {}
+        self.mesh = intg.system.ele_map.items()
         
+        self.neles = {}
+        
+        for etype, eles in intg.system.ele_map.items():
+            self.neles[etype] = eles.neles
+
         self.tnext = 0.0
         
         if self.restart:
             pass
         else:
             for i in range(self.nvorts):
-                self.make_vort_chain(intg, 0.0, i)
+                self.make_vortchain(0.0, i)
 
         # add macro and external data
 
+        self.acteddy = acteddy = {}
+        
         for etype, eles in intg.system.ele_map.items():
             eles.add_src_macro('pyfr.plugins.kernels.turbulence','turbulence',
             {'nvmax': nvmax, 'ls': ls, 'ubar': ubar, 'srafac': srafac, 'xin': xin,
@@ -92,76 +88,61 @@ class Turbulence(BasePlugin):
                                f'in broadcast-col fpdtype_t[{nvmax}][{nparams}]',
                                value=acteddy[etype])
                                
-        # keep info for later (dont actually need to do this, can just look at matrix dims)                      
-        for etype, eles in intg.system.ele_map.items():
-            self.neles[etype] = eles.neles
-    
-    def make_vort_chain(self, intg, tinit, vcid, n=2):
+    def make_vortchain(self, tinit, vcid, clen=2):
         t = tinit
-        vort_chain = OrderedDict()
+        vortchain = OrderedDict()
         seed = vcid
         np.random.seed(seed)
-        for i in range(n):
+        for i in range(clen):
+            vid = uuid.uuid1()
             xyz = np.random.uniform(0, 1, 3)
-
             xinit = self.xmin + (self.xmax-self.xmin)*xyz[0]
             yinit = self.ymin + (self.ymax-self.ymin)*xyz[1]
             zinit = self.zmin + (self.zmax-self.zmin)*xyz[2]
-            
-            #print(f'rank={self.rank}, seed={seed}, x={xinit}, y={yinit}, z={zinit}')
-            
-            #tdead = t + (self.xmax-xinit-self.ls)/self.ubar
             tdead = t + (self.xmax-xinit)/self.ubar
-            #print(tdead)
-            
-            vid = uuid.uuid1()
-            epsx = float(np.random.choice([-1,1]))
-            epsy = float(np.random.choice([-1,1]))
-            epsz = float(np.random.choice([-1,1]))
-            vort = {'vcid': vcid, 'vid': vid, 'xinit': xinit, 'yinit': yinit, 'zinit': zinit, 'tinit': t, 'tdead': tdead, 'eps': epsx, 'epsy': epsy, 'epsz': epsz}
-            vort_chain[vid] = vort
-            self.vort_to_buffer(intg, vort)
+            epsx = np.random.choice([-1,1])
+            epsy = np.random.choice([-1,1])
+            epsz = np.random.choice([-1,1])
+            vort = {'vcid': vcid, 'vid': vid, 'xinit': xinit,
+                    'yinit': yinit, 'zinit': zinit, 'tinit': t,
+                    'tdead': tdead, 'epsx': epsx, 'epsy': epsy,
+                    'epsz': epsz}
+            vortchain[vid] = vort
+            self.vort_to_buffer(vort)
             t += tdead
-            
-        self.vorts.append(vort_chain)
+        self.vortchains.append(vortchain)
            
-    def add_vort_to_chain(self, intg, vcid):
-        t = self.vorts[vcid][next(reversed(self.vorts[vcid]))]['tdead']
-        seed = int(t*10000)
-        
+    def vort_to_vortchain(self, vcid):
+        t = self.vortchains[vcid][next(reversed(self.vortchains[vcid]))]['tdead']
+        seed = int(f'{t:.6E}'.split('E')[0].replace('.', ''))
         np.random.seed(seed)
+        vid = uuid.uuid1()
         xyz = np.random.uniform(0, 1, 3)
-        
         xinit = self.xmin
         yinit = self.ymin + (self.ymax-self.ymin)*xyz[1]
         zinit = self.zmin + (self.zmax-self.zmin)*xyz[2]
-        
         tdead = t + (self.xmax-xinit)/self.ubar
-        
-        vid = uuid.uuid1()
-        
-        epsx = float(np.random.choice([-1,1]))
-        epsy = float(np.random.choice([-1,1]))
-        epsz = float(np.random.choice([-1,1]))
-        
-        vort = {'vcid': vcid, 'vid': vid, 'xinit': xinit, 'yinit': yinit, 'zinit': zinit, 'tinit': t, 'tdead': tdead, 'eps': epsx, 'epsy': epsy, 'epsz': epsz}
-        self.vorts[vcid][vid]=vort
-        self.vort_to_buffer(intg, vort)
-       
-    def vort_to_buffer(self, intg, vort):
+        epsx = np.random.choice([-1,1])
+        epsy = np.random.choice([-1,1])
+        epsz = np.random.choice([-1,1])
+        vort = {'vcid': vcid, 'vid': vid, 'xinit': xinit,
+                'yinit': yinit, 'zinit': zinit, 'tinit': t,
+                'tdead': tdead, 'epsx': epsx, 'epsy': epsy,
+                'epsz': epsz}
+        self.vort_to_buffer(vort)
+        self.vortchains[vcid][vid]=vort
+
+    def vort_to_buffer(self, vort):
         for etype, eles in self.mesh:
-
             elestemp = []
-
             box = BoxRegion([vort['xinit']-self.ls,
                              vort['yinit']-self.ls,
                              vort['zinit']-self.ls],
-                            [self.xmax,
+                            [vort['xinit']+self.ls,
                              vort['yinit']+self.ls,
-                             vort['zinit']+self.ls])
-            # need to take intersetction of this box with the injection region                 
+                             vort['zinit']+self.ls])                
             pts = eles.ploc_at_np('upts')
-            pts = np.moveaxis(pts, 1, -1) # required = check with Freddie
+            pts = np.moveaxis(pts, 1, -1)
             inside = box.pts_in_region(pts)
 
             if np.any(inside):
@@ -172,13 +153,15 @@ class Turbulence(BasePlugin):
                 exmax = pts[:,eid,0].max()
                 ts = max(vort['tinit'], vort['tinit'] + ((exmin - vort['xinit'] - self.ls)/self.ubar))
                 te = min(vort['tdead'], ts + (exmax-exmin+2*self.ls)/self.ubar)
-                self.buff[etype].append({'action': 'push', 'vcid': vort['vcid'], 'vid': vort['vid'], 'eid': eid, 'ts': ts, 'te': te, 't': ts}) 
+                self.buff[etype].append({'action': 'push', 'vcid': vort['vcid'],
+                                         'vid': vort['vid'], 'eid': eid, 'ts': ts,
+                                         'te': te, 't': ts}) 
             
             self.buff[etype].append({'action': 'dead', 'vcid': vort['vcid'],
-                                     'vid': vort['vid'], 'ts': vort['tdead'],
-                                     'te': vort['tdead'], 't': vort['tdead']}) 
+                                     't': vort['tdead']})
+                                     
             self.buff[etype].sort(key=lambda x: x["t"])
-                                    
+                               
     def __call__(self, intg):
         
         tcurr = intg.tcurr
@@ -186,7 +169,6 @@ class Turbulence(BasePlugin):
         tfullm = 0
         
         if tcurr+self.dtmargin >= self.tnext:
-            
             for etype, neles in self.neles.items():
                 temp = np.zeros((self.nvmax, self.nparams, neles))
                 # keep track of which vortex entry we are at for each element
@@ -200,28 +182,30 @@ class Turbulence(BasePlugin):
         	            eid = act['eid']
         	            vcid = act['vcid']
         	            vid = act['vid']
-        	            vort = self.vorts[vcid][vid]
+        	            vort = self.vortchains[vcid][vid]
         	            temp[ctemp[eid],0,eid]=vort['xinit']
         	            temp[ctemp[eid],1,eid]=vort['yinit']
         	            temp[ctemp[eid],2,eid]=vort['zinit']
         	            temp[ctemp[eid],3,eid]=vort['tinit']
-        	            temp[ctemp[eid],4,eid]=vort['eps']
         	            temp[ctemp[eid],5,eid]=act['ts']
         	            temp[ctemp[eid],6,eid]=act['te']
+        	            temp[ctemp[eid],4,eid]=vort['epsx']
         	            temp[ctemp[eid],7,eid]=vort['epsy']
         	            temp[ctemp[eid],8,eid]=vort['epsz']
         	            ctemp[eid] += 1
         	            if ctemp[eid] == self.nvmax:
-        	                # record ts associated with maxed out bufer for given etype
-        	                if act['ts'] == self.tnext:
-        	                    print('Increaase nvmax')
+        	                if act['ts'] <= self.tnext:
+        	                    print('Increase nvmax')
+        	                # record ts associated with maxed out temp for given etype
         	                tfull.append(act['ts'])
         	                # break the while loop for that particular element type
         	                break
                     elif act['action'] == 'dead':
     	                vcid = act['vcid']
     	                # add a new vortx to the chain so it doesnt risk running dry
-    	                self.add_vort_to_chain(intg, vcid)
+    	                # note that it will add events to the buffer, but these will all
+    	                # be in the future, so buffloc is still valid
+    	                self.vort_to_vortchain(vcid)
                 
                 # send off the active eddy array
                 self.acteddy[etype].set(temp)
@@ -236,10 +220,9 @@ class Turbulence(BasePlugin):
                 self.buff[etype] = list(filter(lambda x: x['te'] >= tfullm, self.buff[etype]))
             
             # cull old vorts
-            for vortchain in self.vorts:
+            for vortchain in self.vortchains:
                 while list(vortchain.values())[0]['tdead'] < tfullm:
                     vortchain.popitem(last=False)
-                    
-            print(f'Rank {self.rank}, tfullm  {tfullm}')          
+        
             self.tnext = tfullm
     	    
