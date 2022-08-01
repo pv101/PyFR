96700f77765ed13beba7fc291fe2ea2a182f7435
Peter Vincent <p.vincent@imperial.ac.uk>
2022-08-01 11:43:27 +0100

    Cleanups and made so etypes update async.

--- pyfr/plugins/turbulence.py
+++ pyfr/plugins/turbulence.py
@@ -15,10 +15,10 @@ class Turbulence(BasePlugin):
     systems = ['navier-stokes']
     formulations = ['std']
 
-    def __init__(self, intg, cfgsect, suffix):
+    def __init__(self, intg, cfgsect, suffix, restart=False):
         super().__init__(intg, cfgsect, suffix)
         
-        self.restart = False
+        self.restart = restart
         
         self.comm, self.rank, self.root = get_comm_rank_root()
         constants = self.cfg.items_as('constants', float)
@@ -52,12 +52,14 @@ class Turbulence(BasePlugin):
 
         self.nvmax = nvmax = 15
         self.nparams = nparams = 9
-        
+
         self.vortchains = []
-        
         self.buff = {}
+        self.tfull = []
+        self.etypeupdate = {}
         for etype in intg.system.ele_map:
             self.buff[etype] = []
+            self.etypeupdate[etype] = True
         
         self.mesh = intg.system.ele_map.items()
         
@@ -88,37 +90,13 @@ class Turbulence(BasePlugin):
                                f'in broadcast-col fpdtype_t[{nvmax}][{nparams}]',
                                value=acteddy[etype])
                                
-    def make_vortchain(self, tinit, vcid, clen=2):
-        t = tinit
-        vortchain = OrderedDict()
-        seed = vcid
-        np.random.seed(seed)
-        for i in range(clen):
-            vid = uuid.uuid1()
-            xyz = np.random.uniform(0, 1, 3)
-            xinit = self.xmin + (self.xmax-self.xmin)*xyz[0]
-            yinit = self.ymin + (self.ymax-self.ymin)*xyz[1]
-            zinit = self.zmin + (self.zmax-self.zmin)*xyz[2]
-            tdead = t + (self.xmax-xinit)/self.ubar
-            epsx = np.random.choice([-1,1])
-            epsy = np.random.choice([-1,1])
-            epsz = np.random.choice([-1,1])
-            vort = {'vcid': vcid, 'vid': vid, 'xinit': xinit,
-                    'yinit': yinit, 'zinit': zinit, 'tinit': t,
-                    'tdead': tdead, 'epsx': epsx, 'epsy': epsy,
-                    'epsz': epsz}
-            vortchain[vid] = vort
-            self.vort_to_buffer(vort)
-            t += tdead
-        self.vortchains.append(vortchain)
-           
-    def vort_to_vortchain(self, vcid):
-        t = self.vortchains[vcid][next(reversed(self.vortchains[vcid]))]['tdead']
-        seed = int(f'{t:.6E}'.split('E')[0].replace('.', ''))
-        np.random.seed(seed)
+    def make_vort(self, initial=True)
         vid = uuid.uuid1()
         xyz = np.random.uniform(0, 1, 3)
-        xinit = self.xmin
+        if initial:
+            xinit = self.xmin + (self.xmax-self.xmin)*xyz[0]
+        else:
+            xinit = self.xmin
         yinit = self.ymin + (self.ymax-self.ymin)*xyz[1]
         zinit = self.zmin + (self.zmax-self.zmin)*xyz[2]
         tdead = t + (self.xmax-xinit)/self.ubar
@@ -129,6 +107,31 @@ class Turbulence(BasePlugin):
                 'yinit': yinit, 'zinit': zinit, 'tinit': t,
                 'tdead': tdead, 'epsx': epsx, 'epsy': epsy,
                 'epsz': epsz}
+        return vort
+
+    def make_vortchain(self, tinit, vcid, clen=2):
+        t = tinit
+        vortchain = OrderedDict()
+        seed = vcid
+        np.random.seed(seed)
+        for i in range(clen):
+            vort = make_vort()
+            vortchain[vid] = vort
+            self.vort_to_buffer(vort)
+            t += tdead
+        self.vortchains.append(vortchain)
+        
+    def load_vortchain(self, vortchains):
+        self.vortchains = vortchains
+        for vortchain in self.vortchains:
+            for vort in vortchain:
+                self.vort_to_buffer(vort)
+           
+    def vort_to_vortchain(self, vcid):
+        t = self.vortchains[vcid][next(reversed(self.vortchains[vcid]))]['tdead']
+        seed = int(f'{t:.6E}'.split('E')[0].replace('.', ''))
+        np.random.seed(seed)
+        vort = make_vort(False)
         self.vort_to_buffer(vort)
         self.vortchains[vcid][vid]=vort
 
@@ -165,64 +168,67 @@ class Turbulence(BasePlugin):
     def __call__(self, intg):
         
         tcurr = intg.tcurr
-        tfull = []
-        tfullm = 0
-        
+
         if tcurr+self.dtmargin >= self.tnext:
-            for etype, neles in self.neles.items():
-                temp = np.zeros((self.nvmax, self.nparams, neles))
-                # keep track of which vortex entry we are at for each element
-                ctemp = np.zeros(neles).astype(int)
-                # position along buffer 
-                buffloc = 0
-                while self.buff[etype]:
-                    act = self.buff[etype][buffloc]
-                    buffloc += 1
-                    if act['action'] == 'push':
-        	            eid = act['eid']
-        	            vcid = act['vcid']
-        	            vid = act['vid']
-        	            vort = self.vortchains[vcid][vid]
-        	            temp[ctemp[eid],0,eid]=vort['xinit']
-        	            temp[ctemp[eid],1,eid]=vort['yinit']
-        	            temp[ctemp[eid],2,eid]=vort['zinit']
-        	            temp[ctemp[eid],3,eid]=vort['tinit']
-        	            temp[ctemp[eid],5,eid]=act['ts']
-        	            temp[ctemp[eid],6,eid]=act['te']
-        	            temp[ctemp[eid],4,eid]=vort['epsx']
-        	            temp[ctemp[eid],7,eid]=vort['epsy']
-        	            temp[ctemp[eid],8,eid]=vort['epsz']
-        	            ctemp[eid] += 1
-        	            if ctemp[eid] == self.nvmax:
-        	                if act['ts'] <= self.tnext:
-        	                    print('Increase nvmax')
-        	                # record ts associated with maxed out temp for given etype
-        	                tfull.append(act['ts'])
-        	                # break the while loop for that particular element type
-        	                break
-                    elif act['action'] == 'dead':
-    	                vcid = act['vcid']
-    	                # add a new vortx to the chain so it doesnt risk running dry
-    	                # note that it will add events to the buffer, but these will all
-    	                # be in the future, so buffloc is still valid
-    	                self.vort_to_vortchain(vcid)
-                
-                # send off the active eddy array
-                self.acteddy[etype].set(temp)
-                
-            # agree on how far we can go
-            
-            #get limiting one
-            tfullm = min(tfull, default=0)
-                
-            # cull old actions
-            for etype, neles in self.neles.items():
-                self.buff[etype] = list(filter(lambda x: x['te'] >= tfullm, self.buff[etype]))
-            
-            # cull old vorts
+            # cull vorts from vortchains
             for vortchain in self.vortchains:
-                while list(vortchain.values())[0]['tdead'] < tfullm:
+                while list(vortchain.values())[0]['tdead'] < self.tnext:
                     vortchain.popitem(last=False)
-        
-            self.tnext = tfullm
-    	    
+            for etype, neles in self.neles.items():
+                # check if etype needs updating        
+                if self.etypeupdate[etype]:
+                    print(f'Updating buffer for {etype} on rank {self.rank} for time {self.tnext}')
+                    temp = np.zeros((self.nvmax, self.nparams, neles))
+                    # keep track of which vortex entry we are at for each element
+                    ctemp = np.zeros(neles).astype(int)
+                    # position along buffer 
+                    buffloc = 0
+                    while self.buff[etype]:
+                        act = self.buff[etype][buffloc]
+                        buffloc += 1
+                        if act['action'] == 'push':
+            	            eid = act['eid']
+            	            vcid = act['vcid']
+            	            vid = act['vid']
+            	            vort = self.vortchains[vcid][vid]
+            	            temp[ctemp[eid],0,eid]=vort['xinit']
+            	            temp[ctemp[eid],1,eid]=vort['yinit']
+            	            temp[ctemp[eid],2,eid]=vort['zinit']
+            	            temp[ctemp[eid],3,eid]=vort['tinit']
+            	            temp[ctemp[eid],5,eid]=act['ts']
+            	            temp[ctemp[eid],6,eid]=act['te']
+            	            temp[ctemp[eid],4,eid]=vort['epsx']
+            	            temp[ctemp[eid],7,eid]=vort['epsy']
+            	            temp[ctemp[eid],8,eid]=vort['epsz']
+            	            ctemp[eid] += 1
+            	            if ctemp[eid] == self.nvmax:
+            	                if act['ts'] <= self.tnext:
+            	                    print('Increase nvmax')
+            	                # record ts associated with maxed out temp for given etype
+            	                self.tfull.append({'etype': etype, 't': act['ts']})
+            	                self.tfull.sort(key=lambda x: x["t"])
+            	                self.buff[etype] = list(filter(lambda x: x['te'] >= act['ts'], self.buff[etype]))
+            	                
+            	                # break the while loop for that particular element type
+            	                break
+                        elif act['action'] == 'dead':
+        	                vcid = act['vcid']
+        	                # add a new vortx to the chain so it doesnt risk running dry
+        	                # note that it will add events to the buffer, but these will all
+        	                # be in the future, so buffloc is still valid
+        	                self.vort_to_vortchain(vcid)
+        	                
+                    self.acteddy[etype].set(temp)
+                    self.etypeupdate[etype] = False
+                    
+            # make flags
+            ac = self.tfull.pop(0)
+            self.tnext = ac['t']
+            self.etypeupdate[ac['etype']] = True
+            while self.tfull:
+                if self.tfull[0]['t'] == ac:
+                    ac = self.tfull.pop(0)
+                    self.etypeupdate[ac['etype']] = True
+                else:
+                    break
+
