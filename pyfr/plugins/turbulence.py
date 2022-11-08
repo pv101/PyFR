fb75ce161715ca27d0e7c23a747a0fc6faf9baa2
Peter Vincent <p.vincent@imperial.ac.uk>
2022-11-08 22:00:54 +0000

    Cleanups.

--- pyfr/plugins/turbulence.py
+++ pyfr/plugins/turbulence.py
@@ -24,8 +24,12 @@ class Turbulence(BasePlugin):
         self.tnext = intg.tcurr
         self.tend = intg.tend
 
-        self.fdptype = intg.backend.fpdtype
+        fdptype = intg.backend.fpdtype
 
+        self.vortdtype = np.dtype([('loci', fdptype, 3), ('ti', fdptype), ('eps', fdptype, 3)])
+        self.xttlutdtype = np.dtype([('vid', '<i4'), ('ts', fdptype), ('te', fdptype)])
+        self.buffdtype = np.dtype([('loci', fdptype, 3), ('ti', fdptype), ('eps', fdptype, 3), ('ts', fdptype), ('te', fdptype)])
+        
         btol = 0.1
         nparams = 9
 
@@ -59,41 +63,10 @@ class Turbulence(BasePlugin):
         periodicdim = self.cfg.get(cfgsect, 'periodic-dim', 'none')
 
         c = self.cfg.getliteral(cfgsect, 'centre')
-        e = self.cfg.getliteral(cfgsect, 'rot-axis')
+        e = np.array(self.cfg.getliteral(cfgsect, 'rot-axis'))
 
         shift = np.array(c)
-
-        ear = np.array(e)
-
-        #print(np.cross(ear, np.identity(3) * -1))
-
-        rot=(np.cos(theta)*np.identity(3))
-            +(np.sin(theta)*(np.cross(ear, np.identity(3) * -1)))
-            +(1.0-np.cos(theta))*np.outer(ear,ear)
-
-        # qi = e[0]*np.sin(theta/2) 
-        # qj = e[1]*np.sin(theta/2)
-        # qk = e[2]*np.sin(theta/2)
-        # qr = np.cos(theta/2)
-        
-        # a11 = 1.0 - 2.0*qj*qj - 2.0*qk*qk
-        # a12 = 2.0*(qi*qj - qk*qr)
-        # a13 = 2.0*(qi*qk + qj*qr)
-        # a21 = 2.0*(qi*qj + qk*qr)
-        # a22 = 1.0 - 2.0*qi*qi - 2.0*qk*qk
-        # a23 = 2.0*(qj*qk - qi*qr)
-        # a31 = 2.0*(qi*qk - qj*qr)
-        # a32 = 2.0*(qj*qk + qi*qr)
-        # a33 = 1.0 - 2.0*qi*qi - 2.0*qj*qj
-
-
-        
-        #rot = np.array([[a11, a12, a13],
-        #                [a21, a22, a23],
-        #                [a31, a32, a33]])
-
-        #print(rot-rott)
-        #print(rott)
+        rot=(np.cos(theta)*np.identity(3))+(np.sin(theta)*(np.cross(e, np.identity(3) * -1)))+(1.0-np.cos(theta))*np.outer(e,e)
 
         self.dtol = 0
         
@@ -127,24 +100,22 @@ class Turbulence(BasePlugin):
                 epsy = rng.choice([-1,1])
                 epsz = rng.choice([-1,1])
                 if t >= self.tbegin:
-                    temp.append([xinit,yinit,zinit,t,epsx,epsy,epsz])
                     xtemp.append(((xinit,yinit,zinit),t,(epsx,epsy,epsz)))
                     if periodicdim == 'y':
                         if yinit+ls>ymax:
-                            temp.append([xinit,ymin-(ymax-yinit),zinit,t,epsx,epsy,epsz])
+                            xtemp.append(((xinit,ymin-(ymax-yinit),zinit),t,(epsx,epsy,epsz)))
                         if yinit-ls<ymin:
-                            temp.append([xinit,ymax+(yinit-ymin),zinit,t,epsx,epsy,epsz])
+                            xtemp.append(((xinit,ymax+(yinit-ymin),zinit),t,(epsx,epsy,epsz)))
                     if periodicdim == 'z':
                         if zinit+ls>zmax:
-                            temp.append([xinit,yinit,zmin-(zmax-zinit),t,epsx,epsy,epsz])
+                            xtemp.append(((xinit,yinit,zmin-(zmax-zinit)),t,(epsx,epsy,epsz)))
                         if zinit-ls<zmin:
-                            temp.append([xinit,yinit,zmax+(zinit-zmin),t,epsx,epsy,epsz])
+                            xtemp.append(((xinit,yinit,zmax+(zinit-zmin)),t,(epsx,epsy,epsz)))
                 t += (xmax-xinit)/ubar
                 initial = False
             vid += 1
 
-        self.vortbuff = np.asarray(temp)
-        self.xvortbuff = np.asarray(xtemp, [('loci', self.fdptype, 3), ('ti', self.fdptype), ('eps', self.fdptype, 3)])
+        self.xvortbuff = np.asarray(xtemp, self.vortdtype)
 
         #####################
         # Make action buffer#
@@ -169,9 +140,9 @@ class Turbulence(BasePlugin):
             if np.any(inside):
                 eids = np.any(inside, axis=0).nonzero()[0] # eles in injection box
                 ptsri = ptsr[:,eids,:] # points in injection box
-                for vid, vort in enumerate(self.vortbuff):
-                    vbox = BoxRegion([vort[0]-ls, vort[1]-ls, vort[2]-ls],
-                                     [xmax+ls, vort[1]+ls, vort[2]+ls])
+                for vid, vort in enumerate(self.xvortbuff):
+                    vbox = BoxRegion([vort['loci'][0]-ls, vort['loci'][1]-ls, vort['loci'][2]-ls],
+                                     [xmax+ls, vort['loci'][1]+ls, vort['loci'][2]+ls])
                     elestemp = []               
                     vinside = vbox.pts_in_region(ptsri)
 
@@ -181,13 +152,13 @@ class Turbulence(BasePlugin):
                     for eid in elestemp:
                         exmin = ptsri[:,eid,0].min()
                         exmax = ptsri[:,eid,0].max()
-                        ts = max(vort[3], vort[3] + ((exmin - vort[0] - ls)/ubar))
+                        ts = max(vort['ti'], vort['ti'] + ((exmin - vort['loci'][0] - ls)/ubar))
                         te = ts + (exmax-exmin+2*ls)/ubar
                         ttlut[eids[eid]].append((vid,ts,te))
 
                 for k, v in ttlut.items():
                     v.sort(key=lambda x: x[1])
-                    xttlut[k] = np.asarray(v, [('vid', '<i4'), ('ts', self.fdptype), ('te', self.fdptype)])
+                    xttlut[k] = np.asarray(v, self.xttlutdtype)
                     nv = np.array(v)
                     vidx[k] = nv[:,0].astype(int)
                     vtim[k] = nv[:,-2:]
@@ -208,7 +179,7 @@ class Turbulence(BasePlugin):
 
                 #buff = np.full((nvmx, nparams, neles), 0.0)
 
-                buff = np.zeros((nvmx, neles), [('loci', self.fdptype, 3), ('ti', self.fdptype), ('eps', self.fdptype, 3), ('ts', self.fdptype), ('te', self.fdptype)])
+                buff = np.zeros((nvmx, neles), self.buffdtype)
 
 
                 #actbuff = {'trcl': 0.0, 'vidx': vidx, 'vtim': vtim, 'nvmx': nvmx, 'buff': buff, 'acteddy': eles._be.matrix((nvmx, nparams, neles), tags={'align'})}
@@ -242,37 +213,14 @@ class Turbulence(BasePlugin):
                             #geid = actbuff['geid'][leid]
                             shft = next((i for i,v in enumerate(actbuff['buff'][:,geid]['te']) if v > tcurr),actbuff['nvmx'])
                             if shft:
-
-                                newb = np.zeros(shft, [('loci', self.fdptype, 3), ('ti', self.fdptype), ('eps', self.fdptype, 3), ('ts', self.fdptype), ('te', self.fdptype)])
+                                newb = np.zeros(shft, self.buffdtype)
                                 xxxx = self.xvortbuff[xttluts['vid'][:shft]]
                                 pad = shft-xxxx.shape[0]
                                 newb[['loci', 'ti', 'eps']] = np.pad(xxxx, (0,pad), 'constant')
                                 newb[['ts', 'te']] = np.pad(xttluts[['ts', 'te']][:shft], (0,pad), 'constant')
-
-                                #newb = np.concatenate((self.vortbuff[vidxs[:shft],:], actbuff['vtim'][geid][:shft,:]), axis=1)
-                                #newb = np.pad(newb, [(0,shft-newb.shape[0]),(0,0)], 'constant')
-
                                 self.actbuffs[abid]['buff'][:,geid] = np.concatenate((actbuff['buff'][shft:,geid],newb))
                                 self.actbuffs[abid]['xttlut'][geid] = xttluts[shft:]
-
-
-                                #self.actbuffs[abid]['buff'][:,:,geid] = np.concatenate((actbuff['buff'][shft:,:,geid],newb))
-                                #self.actbuffs[abid]['vidx'][geid] = vidxs[shft:]
-                                #self.actbuffs[abid]['vtim'][geid] = actbuff['vtim'][geid][shft:,:]
-
                                 if self.actbuffs[abid]['xttlut'][geid]['vid'].any() and (self.actbuffs[abid]['buff'][-1,geid]['ts'] < trcl):
-                                    trcl = self.actbuffs[abid]['buff'][-1,geid]['ts']
-                    self.actbuffs[abid]['trcl'] = trcl
-
-                    print(actbuff['buff'].shape)
-                    uns = structured_to_unstructured(actbuff['buff'])
-                    print(uns.shape)
-                    uns = np.moveaxis(uns, 2, 1)
-                    print(uns.shape)
-
-                    self.actbuffs[abid]['acteddy'].set(uns)
-
-
-
-
+                                    trcl = self.actbuffs[abid]['buff']['ts'][-1,geid]
+                    self.actbuffs[abid]['acteddy'].set(np.moveaxis(structured_to_unstructured(actbuff['buff']), 2, 1))
             self.tnext = min(etype['trcl'] for etype in self.actbuffs)
