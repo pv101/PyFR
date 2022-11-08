00c5705052f60ebea2139072dfd8ee93cd5c88e6
Peter Vincent <p.vincent@imperial.ac.uk>
2022-11-08 21:03:20 +0000

    Using structured arrays.

--- pyfr/plugins/turbulence.py
+++ pyfr/plugins/turbulence.py
@@ -6,6 +6,7 @@ import random
 import uuid
 
 from collections import defaultdict
+from numpy.lib.recfunctions import structured_to_unstructured
 from pyfr.plugins.base import BasePlugin
 from pyfr.regions import BoxRegion, RotatedBoxRegion
 from pyfr.mpiutil import get_comm_rank_root
@@ -62,24 +63,37 @@ class Turbulence(BasePlugin):
 
         shift = np.array(c)
 
-        qi = e[0]*np.sin(theta/2) 
-        qj = e[1]*np.sin(theta/2)
-        qk = e[2]*np.sin(theta/2)
-        qr = np.cos(theta/2)
+        ear = np.array(e)
+
+        #print(np.cross(ear, np.identity(3) * -1))
+
+        rot=(np.cos(theta)*np.identity(3))
+            +(np.sin(theta)*(np.cross(ear, np.identity(3) * -1)))
+            +(1.0-np.cos(theta))*np.outer(ear,ear)
+
+        # qi = e[0]*np.sin(theta/2) 
+        # qj = e[1]*np.sin(theta/2)
+        # qk = e[2]*np.sin(theta/2)
+        # qr = np.cos(theta/2)
         
-        a11 = 1.0 - 2.0*qj*qj - 2.0*qk*qk
-        a12 = 2.0*(qi*qj - qk*qr)
-        a13 = 2.0*(qi*qk + qj*qr)
-        a21 = 2.0*(qi*qj + qk*qr)
-        a22 = 1.0 - 2.0*qi*qi - 2.0*qk*qk
-        a23 = 2.0*(qj*qk - qi*qr)
-        a31 = 2.0*(qi*qk - qj*qr)
-        a32 = 2.0*(qj*qk + qi*qr)
-        a33 = 1.0 - 2.0*qi*qi - 2.0*qj*qj
+        # a11 = 1.0 - 2.0*qj*qj - 2.0*qk*qk
+        # a12 = 2.0*(qi*qj - qk*qr)
+        # a13 = 2.0*(qi*qk + qj*qr)
+        # a21 = 2.0*(qi*qj + qk*qr)
+        # a22 = 1.0 - 2.0*qi*qi - 2.0*qk*qk
+        # a23 = 2.0*(qj*qk - qi*qr)
+        # a31 = 2.0*(qi*qk - qj*qr)
+        # a32 = 2.0*(qj*qk + qi*qr)
+        # a33 = 1.0 - 2.0*qi*qi - 2.0*qj*qj
+
+
         
-        rot = np.array([[a11, a12, a13],
-                        [a21, a22, a23],
-                        [a31, a32, a33]])
+        #rot = np.array([[a11, a12, a13],
+        #                [a21, a22, a23],
+        #                [a31, a32, a33]])
+
+        #print(rot-rott)
+        #print(rott)
 
         self.dtol = 0
         
@@ -97,6 +111,8 @@ class Turbulence(BasePlugin):
         
         vid = 0
         temp = []
+        xtemp = []
+
         while vid <= nvorts:
             t = self.tstart # start right at the start
             initial = True
@@ -112,6 +128,7 @@ class Turbulence(BasePlugin):
                 epsz = rng.choice([-1,1])
                 if t >= self.tbegin:
                     temp.append([xinit,yinit,zinit,t,epsx,epsy,epsz])
+                    xtemp.append(((xinit,yinit,zinit),t,(epsx,epsy,epsz)))
                     if periodicdim == 'y':
                         if yinit+ls>ymax:
                             temp.append([xinit,ymin-(ymax-yinit),zinit,t,epsx,epsy,epsz])
@@ -127,6 +144,7 @@ class Turbulence(BasePlugin):
             vid += 1
 
         self.vortbuff = np.asarray(temp)
+        self.xvortbuff = np.asarray(xtemp, [('loci', self.fdptype, 3), ('ti', self.fdptype), ('eps', self.fdptype, 3)])
 
         #####################
         # Make action buffer#
@@ -144,6 +162,7 @@ class Turbulence(BasePlugin):
             inside = bbox.pts_in_region(ptsr)
 
             ttlut = defaultdict(list)
+            xttlut = defaultdict()
             vidx = defaultdict()
             vtim = defaultdict()
 
@@ -164,26 +183,37 @@ class Turbulence(BasePlugin):
                         exmax = ptsri[:,eid,0].max()
                         ts = max(vort[3], vort[3] + ((exmin - vort[0] - ls)/ubar))
                         te = ts + (exmax-exmin+2*ls)/ubar
-                        ttlut[eid].append([vid,ts,te])
+                        ttlut[eids[eid]].append((vid,ts,te))
 
                 for k, v in ttlut.items():
                     v.sort(key=lambda x: x[1])
+                    xttlut[k] = np.asarray(v, [('vid', '<i4'), ('ts', self.fdptype), ('te', self.fdptype)])
                     nv = np.array(v)
                     vidx[k] = nv[:,0].astype(int)
                     vtim[k] = nv[:,-2:]
 
+                # nvmx = 0
+                # for leid, actl in vtim.items():
+                #     for i, te in enumerate(actl[:,1]):
+                #         shft = next((j for j,v in enumerate(actl[:,0]) if v > te+btol),len(actl)-1) - i + 1
+                #         if shft > nvmx:
+                #             nvmx = shft
+
                 nvmx = 0
-                for leid, actl in vtim.items():
-                    for i, te in enumerate(actl[:,1]):
-                        shft = next((j for j,v in enumerate(actl[:,0]) if v > te+btol),len(actl)-1) - i + 1
+                for leid, actl in xttlut.items():
+                    for i, te in enumerate(actl['te']):
+                        shft = next((j for j,v in enumerate(actl['ts']) if v > te+btol),len(actl)-1) - i + 1
                         if shft > nvmx:
                             nvmx = shft
 
-                buff = np.full((nvmx, nparams, neles), 0.0)
-                actbuff = {'geid': eids, 'trcl': 0.0, 'neles': neles, 'etype': etype, 'vidx': vidx, 'vtim': vtim, 'nvmx': nvmx, 'buff': buff, 'acteddy': eles._be.matrix((nvmx, nparams, neles), tags={'align'})}
+                #buff = np.full((nvmx, nparams, neles), 0.0)
 
-                print(c)
-                print(shift)
+                buff = np.zeros((nvmx, neles), [('loci', self.fdptype, 3), ('ti', self.fdptype), ('eps', self.fdptype, 3), ('ts', self.fdptype), ('te', self.fdptype)])
+
+
+                #actbuff = {'trcl': 0.0, 'vidx': vidx, 'vtim': vtim, 'nvmx': nvmx, 'buff': buff, 'acteddy': eles._be.matrix((nvmx, nparams, neles), tags={'align'})}
+
+                actbuff = {'trcl': 0.0, 'xttlut': xttlut, 'nvmx': nvmx, 'buff': buff, 'acteddy': eles._be.matrix((nvmx, nparams, neles), tags={'align'})}
 
                 eles.add_src_macro('pyfr.plugins.kernels.turbulence','turbulence',
                 {'nvmax': nvmx, 'ls': ls, 'ubar': ubar, 'srafac': srafac,
@@ -207,18 +237,42 @@ class Turbulence(BasePlugin):
             for abid, actbuff in enumerate(self.actbuffs):    
                 if actbuff['trcl'] <= self.tnext:
                     trcl = np.inf
-                    for leid, vidxs in actbuff['vidx'].items():
-                        if vidxs.any():
-                            geid = actbuff['geid'][leid]
-                            shft = next((i for i,v in enumerate(actbuff['buff'][:,8,geid]) if v > tcurr),actbuff['nvmx'])
+                    for geid, xttluts in actbuff['xttlut'].items():
+                        if xttluts['vid'].any():
+                            #geid = actbuff['geid'][leid]
+                            shft = next((i for i,v in enumerate(actbuff['buff'][:,geid]['te']) if v > tcurr),actbuff['nvmx'])
                             if shft:
-                                newb = np.concatenate((self.vortbuff[vidxs[:shft],:], actbuff['vtim'][leid][:shft,:]), axis=1)
-                                newb = np.pad(newb, [(0,shft-newb.shape[0]),(0,0)], 'constant')
-                                self.actbuffs[abid]['buff'][:,:,geid] = np.concatenate((actbuff['buff'][shft:,:,geid],newb))
-                                self.actbuffs[abid]['vidx'][leid] = vidxs[shft:]
-                                self.actbuffs[abid]['vtim'][leid] = actbuff['vtim'][leid][shft:,:]
-                                if self.actbuffs[abid]['vidx'][leid].any() and (self.actbuffs[abid]['buff'][-1,7,geid] < trcl):
-                                    trcl = self.actbuffs[abid]['buff'][-1,7,geid]
+
+                                newb = np.zeros(shft, [('loci', self.fdptype, 3), ('ti', self.fdptype), ('eps', self.fdptype, 3), ('ts', self.fdptype), ('te', self.fdptype)])
+                                xxxx = self.xvortbuff[xttluts['vid'][:shft]]
+                                pad = shft-xxxx.shape[0]
+                                newb[['loci', 'ti', 'eps']] = np.pad(xxxx, (0,pad), 'constant')
+                                newb[['ts', 'te']] = np.pad(xttluts[['ts', 'te']][:shft], (0,pad), 'constant')
+
+                                #newb = np.concatenate((self.vortbuff[vidxs[:shft],:], actbuff['vtim'][geid][:shft,:]), axis=1)
+                                #newb = np.pad(newb, [(0,shft-newb.shape[0]),(0,0)], 'constant')
+
+                                self.actbuffs[abid]['buff'][:,geid] = np.concatenate((actbuff['buff'][shft:,geid],newb))
+                                self.actbuffs[abid]['xttlut'][geid] = xttluts[shft:]
+
+
+                                #self.actbuffs[abid]['buff'][:,:,geid] = np.concatenate((actbuff['buff'][shft:,:,geid],newb))
+                                #self.actbuffs[abid]['vidx'][geid] = vidxs[shft:]
+                                #self.actbuffs[abid]['vtim'][geid] = actbuff['vtim'][geid][shft:,:]
+
+                                if self.actbuffs[abid]['xttlut'][geid]['vid'].any() and (self.actbuffs[abid]['buff'][-1,geid]['ts'] < trcl):
+                                    trcl = self.actbuffs[abid]['buff'][-1,geid]['ts']
                     self.actbuffs[abid]['trcl'] = trcl
-                    self.actbuffs[abid]['acteddy'].set(actbuff['buff'])
+
+                    print(actbuff['buff'].shape)
+                    uns = structured_to_unstructured(actbuff['buff'])
+                    print(uns.shape)
+                    uns = np.moveaxis(uns, 2, 1)
+                    print(uns.shape)
+
+                    self.actbuffs[abid]['acteddy'].set(uns)
+
+
+
+
             self.tnext = min(etype['trcl'] for etype in self.actbuffs)
